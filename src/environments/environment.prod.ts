35f751868f76cc797e3272272d75291f9392005c
Mustapha Adewale <MAdewale@gcn.ab-inbev.com>
2020-10-14 18:44:04 +0100

    Done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   category_url: 'https://uplanit-test-api.herokuapp.com/public/category',
-  event_url: 'https://uplanit-test-api.herokuapp.com/public/event-types'
+  event_url: 'https://uplanit-test-api.herokuapp.com/public/event-types',
+  be_url: 'https://uplanit-test-endpoint/category'
+ 
 };
